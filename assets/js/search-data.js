85bb7ab1eeb445c6a0ffcfa887a8375a2e406e61
ther3lab <ther3lab@gmail.com>
2025-06-12 23:23:09 +0000

    Deploying to gh-pages from @ ther3lab/ther3lab.github.io@0391e992297110241051581cc719d72ce65003de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,8 +55,8 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-what-role-does-functional-diversity-play-in-regulation",
+          title: 'What Role does Functional Diversity Play in Regulation?',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
